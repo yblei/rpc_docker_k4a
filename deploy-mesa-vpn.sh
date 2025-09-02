f6e14d300f6d50a0b444b519d25860a194af02b9
Yannik Blei <yannik.blei@utn.de>
2025-09-02 20:51:32 +0200

    some more cleanup

--- deploy-mesa-vpn.sh
+++ deploy-mesa-vpn.sh
@@ -13,6 +13,7 @@ docker run -it --rm \
     -v /dev:/dev:rw \
     -v /etc/udev/rules.d:/etc/udev/rules.d:rw \
     -v $(pwd)/99-k4a.rules:/etc/udev/rules.d/99-k4a.rules:ro \
+    -v $(pwd):/test:rw \
     --device-cgroup-rule='c 81:* rmw' \
     --device-cgroup-rule='c 189:* rmw' \
     azure-kinect-mesa-vpn \
